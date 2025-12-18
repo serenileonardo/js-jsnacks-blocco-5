85cba7a406296e4c31430e550f9a6bd8a026a257
serenileonardo <serenileonardo.web@gmail.com>
2025-12-18 20:06:38 +0100

    feat: push snack 7

--- snack-7/script.js
+++ snack-7/script.js
@@ -21,3 +21,7 @@ const students = [
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+
+const over30 = students.filter(student => student.age > 30);
+
+console.log(over30);
