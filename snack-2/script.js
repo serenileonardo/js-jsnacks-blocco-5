3b6ade641cf4102e365e7fac8bd9195eafdbae08
serenileonardo <serenileonardo.web@gmail.com>
2025-12-18 20:05:00 +0100

    fix: fix code from 1 to 5

--- snack-2/script.js
+++ snack-2/script.js
@@ -7,10 +7,5 @@ const people = [
 // Stampa in console tutti i nomi
 // Risultato: 'Paolo', 'Giulia', 'Marco'
 
-const people = [
-  { name: 'Paolo', age: 35 },
-  { name: 'Giulia', age: 24 },
-  { name: 'Marco', age: 67 }
-];
 
 people.forEach(person => console.log(person.name));
