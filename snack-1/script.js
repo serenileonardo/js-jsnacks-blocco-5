5f7e652bab820d525b4077c95c954d7ea9304edc
serenileonardo <serenileonardo.web@gmail.com>
2025-12-18 19:59:46 +0100

    feat: push snack 1

--- snack-1/script.js
+++ snack-1/script.js
@@ -3,6 +3,6 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
 
-const names = ['Edoardo', 'Simone', 'Francesco'];
 
 names.forEach(name => console.log(name));
+
