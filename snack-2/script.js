0b6e0e2dacb75cb59dc9649256f2231093ca7cf7
serenileonardo <serenileonardo.web@gmail.com>
2025-12-18 20:00:59 +0100

    feat: push nasck 2

--- snack-2/script.js
+++ snack-2/script.js
@@ -6,3 +6,11 @@ const people = [
 
 // Stampa in console tutti i nomi
 // Risultato: 'Paolo', 'Giulia', 'Marco'
+
+const people = [
+  { name: 'Paolo', age: 35 },
+  { name: 'Giulia', age: 24 },
+  { name: 'Marco', age: 67 }
+];
+
+people.forEach(person => console.log(person.name));
