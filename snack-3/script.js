029be7bb1662d328d3672ab93d4e612ca6897ab8
serenileonardo <serenileonardo.web@gmail.com>
2025-12-18 20:01:56 +0100

    feat: push snack 3

--- snack-3/script.js
+++ snack-3/script.js
@@ -2,3 +2,9 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+const numbers = [2, 8, 4, 7, 2, 87];
+
+const incrementedNumbers = numbers.map(num => num + 1);
+
+console.log(incrementedNumbers);
